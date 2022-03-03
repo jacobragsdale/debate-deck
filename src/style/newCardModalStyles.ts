71d021a9cb2618d1965e9e1b6ec7886db6438ce3
Eli <52673344+EliBP@users.noreply.github.com>
2022-03-03 11:12:00 -0600

    initial commit

--- src/style/newCardModalStyles.ts
+++ src/style/newCardModalStyles.ts
@@ -3,35 +3,63 @@ import DDUtils from '../components/DDUtils'
 
 const theme = DDUtils.getProjectTheme()
 
-export const landerStyles = makeStyles({
+export const newCardModalStyles = makeStyles({
     root: {
-        background: theme.palette.background.default,
-        width: '100%',
-        height: '100%',
-        position: 'absolute',
-        padding: theme.spacing(3),
-        display: 'flex',
-        flexDirection: 'column',
+
     },
-    actionContainer: {
-        minHeight: '300px',
-        alignContent: 'center',
-        display: 'flex',
-    },
-    card: {
-        margin: theme.spacing(2),
-        padding: theme.spacing(2),
+    box: {
         borderRadius: 24,
-        background: theme.palette.background.paper,
+        margin: theme.spacing(1),
+        minWidth: 800,
+        alignItems: "center",
+        display: "flex",
+        flexwrap: "wrap",
+    },
+    textColumn: {
+        minWidth: 250,
+        flexDirection: "column",
+        alignItems: "center",
+        display: "flex",
+        padding: theme.spacing(1),
+    },
+    quoteColumn: {
+        margin: theme.spacing(1,0),
+        alignItems: "center",
+        justifyContent: 'flex-start',
+        display: "flex",
+        flexDirection: "column",
+        flexGrow: 1
+
     },
     title: {
         color: theme.palette.text.primary,
     },
-    button: {
+    saveButton: {
         background: theme.palette.primary.main,
         borderRadius: 10,
-        minWidth: 130,
+        width: 100,
         minHeight: 40,
-        margin: 'auto',
+        margin: theme.spacing(1),
     },
+    closeButton: {
+        color: theme.palette.error.light,
+        borderRadius: 10,
+        minWidth: 100,
+        minHeight: 40,
+        margin: theme.spacing(1),
+    },
+    textField: {
+        margin: theme.spacing(.5,1),
+        width: "100%"
+    },
+    quoteField: {
+        margin: theme.spacing(1,2),
+        width: "100%"
+    },
+    proConDiv: {
+        height: 62,
+        alignItems: "center",
+        display: "flex",
+
+    }
 })
